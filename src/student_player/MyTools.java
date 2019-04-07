33e55ab56004d8eb28f11687e959642f7650ff33
abraham yesgat <abraham.yesgat@mail.mcgill.ca>
2019-04-07 12:36:01 -0400

    sunday
    sunday

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -48,6 +48,7 @@ public class MyTools {
         return myMove ;
 
     }
+<<<<<<< HEAD
     public static void setCoords(PentagoBoardState boardState){
         List<PentagoCoord> coordsA = Arrays.asList(new PentagoCoord(0,4), new PentagoCoord(1,3), new PentagoCoord(2,2), new PentagoCoord(3,1), new PentagoCoord(4,0));
         List<PentagoCoord> coordsB = Arrays.asList(new PentagoCoord(0,5), new PentagoCoord(1,4), new PentagoCoord(2,3), new PentagoCoord(3,2), new PentagoCoord(4,1), new PentagoCoord(5,0));
@@ -63,6 +64,118 @@ public class MyTools {
         diagF.addAll(coordsF);
 
 
+=======
+    public static PentagoBoardState.Quadrant getQuadrant(int row, int col){
+        if(row<=2 && col<=2){
+            return PentagoBoardState.Quadrant.TL;
+        }else if(row > 2 && col<=2){
+            return PentagoBoardState.Quadrant.BL;
+        }else if(row > 2 && col>2){
+            return PentagoBoardState.Quadrant.BL;
+        }else{
+            return PentagoBoardState.Quadrant.TR;
+        }
+    }
+    public static PentagoBoardState.Quadrant getDiagonalQuadrant(PentagoBoardState.Quadrant quad){
+        if(quad== PentagoBoardState.Quadrant.TL){
+            return PentagoBoardState.Quadrant.BR;
+        }else if(quad== PentagoBoardState.Quadrant.BR){
+            return PentagoBoardState.Quadrant.TL;
+        }else if(quad== PentagoBoardState.Quadrant.BL){
+            return PentagoBoardState.Quadrant.TR;
+        }else{
+            return PentagoBoardState.Quadrant.BL;
+        }
+
+    }
+    public static PentagoBoardState.Quadrant getXNeighborQuadrant(PentagoBoardState.Quadrant quad){
+        if(quad == PentagoBoardState.Quadrant.TL){
+            return PentagoBoardState.Quadrant.TR;
+        }else if(quad== PentagoBoardState.Quadrant.BR){
+            return PentagoBoardState.Quadrant.BL;
+        }else if(quad== PentagoBoardState.Quadrant.BL){
+            return PentagoBoardState.Quadrant.BR;
+        }else{
+            return PentagoBoardState.Quadrant.TL;
+        }
+
+    }
+    public static PentagoBoardState.Quadrant getYNeighborQuadrant(PentagoBoardState.Quadrant quad){
+        if(quad == PentagoBoardState.Quadrant.TL){
+            return PentagoBoardState.Quadrant.BL;
+        }else if(quad== PentagoBoardState.Quadrant.BR){
+            return PentagoBoardState.Quadrant.TR;
+        }else if(quad== PentagoBoardState.Quadrant.BL){
+            return PentagoBoardState.Quadrant.TL;
+        }else{
+            return PentagoBoardState.Quadrant.BR;
+        }
+
+    }
+    public static PentagoCoord getDefenseMovesInRow(PentagoBoardState boardState, int row){
+        ArrayList<PentagoMove> sameRow = new ArrayList<PentagoMove>();
+        for(PentagoMove move : boardState.getAllLegalMoves()){
+            if(move.getMoveCoord().getX() == row) sameRow.add(move);
+        }
+
+        if(boardState.getPieceAt(row, 0) == PentagoBoardState.Piece.EMPTY){
+            System.out.println("Right Aligned Danger");
+            return getMaxX(sameRow).getMoveCoord();
+        }
+        else{
+            System.out.println("Left Aligned Danger");
+            return getMinX(sameRow).getMoveCoord();
+        }
+    }
+    public static PentagoCoord getDefenseMovesInCol(PentagoBoardState boardState, int col){
+        ArrayList<PentagoMove> sameCol = new ArrayList<PentagoMove>();
+        ArrayList<PentagoMove> moves = boardState.getAllLegalMoves();
+        for(PentagoMove move : moves){
+            if(move.getMoveCoord().getY() == col) sameCol.add(move);
+        }
+        if((boardState.getPieceAt(0,col) == (PentagoBoardState.Piece.EMPTY))){
+            return getMaxY(sameCol).getMoveCoord();
+        }
+        else{
+            return getMinY(sameCol).getMoveCoord();
+        }
+    }
+    public static PentagoMove getMinY(ArrayList<PentagoMove> moves) {
+        PentagoMove minY = moves.get(0);
+        for (PentagoMove m : moves) {
+            if (m.getMoveCoord().getY() < minY.getMoveCoord().getY()) {
+                minY = m;
+            }
+        }
+        return minY;
+    }
+    public static PentagoMove getMinX(ArrayList<PentagoMove> moves) {
+        PentagoMove minX = moves.get(0);
+        for (PentagoMove m : moves) {
+            if (m.getMoveCoord().getX() < minX.getMoveCoord().getX()) {
+                minX = m;
+            }
+        }
+        return minX;
+    }
+    public static PentagoMove getMaxX (ArrayList<PentagoMove> moves){
+        PentagoMove maxX = moves.get(0);
+        for (PentagoMove m : moves) {
+            if (m.getMoveCoord().getX() > maxX.getMoveCoord().getX()) {
+                maxX = m;
+            }
+        }
+        return maxX;
+    }
+    public static PentagoMove getMaxY(ArrayList<PentagoMove> moves) {
+        PentagoMove maxY = moves.get(0);
+        for (PentagoMove m : moves) {
+            if (m.getMoveCoord().getY() > maxY.getMoveCoord().getY()) {
+                maxY = m;
+            }
+        }
+        return maxY;
+>>>>>>> parent of b47362f... BETA DONE
     }
 
 
@@ -96,6 +209,7 @@ public class MyTools {
                 myMove = m;
             }
         }
+<<<<<<< HEAD
         plusMinus = 0;
         for(PentagoMove m : midMoves){
             previous = plusMinus;
@@ -113,6 +227,21 @@ public class MyTools {
 
 
 
+=======
+        return myMove;
+//        plusMinus = 0;
+//        for(PentagoMove m : midMoves){
+//            previous = plusMinus;
+//            plusMinus = 0;
+//            for(int i = 0 ; i < simsMid; i++){
+//                plusMinus += simulateGame(boardState,m);
+//            }
+//            winRateMids = plusMinus /simsMid;
+//            if(plusMinus > previous){
+//                myMoveMid = m;
+//            }
+//        }
+>>>>>>> parent of b47362f... BETA DONE
 //        plusMinus = 0;
 //        for(PentagoMove m : lowerMoves){
 //            previous = plusMinus;
@@ -131,7 +260,12 @@ public class MyTools {
 //            System.out.println("Chose a lower wit win rate: " + winRateLower);
 //            return myMoveLower;
 //        } else
-
+//            if(max == winRateMids){
+//            System.out.println("Chose a mid wit win rate: " + winRateMids);
+//            return myMoveMid;
+//        }else{
+//            return myMove;
+//        }
 //        System.out.println("The win rate of this move is: " + winrate + "based on " + total + " total  games" );
     }
 
@@ -195,14 +329,11 @@ public class MyTools {
     public static double simulateGame(PentagoBoardState boardState , PentagoMove move){
         PentagoBoardState boardStateClone = (PentagoBoardState)boardState.clone();
         boardStateClone.processMove(move);
-        int levels = 5;
-
-        while(boardStateClone.getWinner() == Board.NOBODY && levels > 0 ){
+        while(boardStateClone.getWinner() == Board.NOBODY ){
             HashMap<PentagoMove, Integer> map = heuristicFunctionMap(boardStateClone);
                 ArrayList<PentagoMove> moves = getTopMoves(map);
                 move = getRandomTopMove(moves);
                 (boardStateClone).processMove(move);
-                levels --;
         }
 
         return evaluateGame(boardStateClone);
@@ -226,6 +357,7 @@ public class MyTools {
         }
 
     }
+<<<<<<< HEAD
     public static int getHeuristic(PentagoMove move, PentagoBoardState boardState){
         PentagoBoardState boardStateClone = (PentagoBoardState)boardState.clone();
         int rowheuristic = getPiecesInRow(boardStateClone, move.getMoveCoord().getX());
@@ -234,6 +366,16 @@ public class MyTools {
         int max = Math.max(diagheuristic, Math.max(rowheuristic,colheuristic));
 
         return Math.max(diagheuristic, Math.max(rowheuristic,colheuristic));
+=======
+    public static int getHeuristic(PentagoMove move, PentagoBoardState boardState ){
+        int rowheuristic = getMyPiecesInRow(boardState, move.getMoveCoord().getX());
+        int colheuristic = getMyPiecesInCol(boardState, move.getMoveCoord().getY());
+//        int rowOppheuristic = getOppPiecesInRow(boardStateClone,move.getMoveCoord().getX());
+//        int colOppheuristic = getOppPiecesInCol(boardStateClone,move.getMoveCoord().getY());
+        int a = Math.max(rowheuristic, colheuristic);
+//        int b = Math.max(rowOppheuristic, colOppheuristic);
+        return a;
+>>>>>>> parent of b47362f... BETA DONE
     }
 
 
@@ -413,6 +555,44 @@ public class MyTools {
             }
         }
         return null;
+    }
+
+    public static PentagoMove checkDanger(PentagoBoardState boardState, PentagoMove move){
+        int opponent = boardState.getOpponent();
+        int count = 0;
+        PentagoBoardState.Piece piece ;
+        if(opponent == 1) {
+            piece = PentagoBoardState.Piece.WHITE;
+        }     else{
+            piece = PentagoBoardState.Piece.BLACK;
+        }       //Check Columns
+        for (int col = 0; col <= 5; col++) {
+                    for (int row = 0; row <= 5; row++) {
+                        if (boardState.getPieceAt(row, col) == piece) count++;
+                        if (count == 3) {
+                            System.out.println("DANGER");
+                            return new PentagoMove(getDefenseMovesInCol(boardState, col), getQuadrant(row, col), getDiagonalQuadrant(getQuadrant(row, col)), boardState.getTurnPlayer());
+                        }
+                    }
+
+                    count =0;
+
+            }
+
+        for (int row = 0; row <= 5; row++){
+            for (int col = 0; col <= 5; col++) {
+                if (boardState.getPieceAt(row, col) == piece)count++;
+                if (count == 3) {
+                    System.out.println("DANGER");
+                    return new PentagoMove(getDefenseMovesInRow(boardState, col), getQuadrant(row, col), getDiagonalQuadrant(getQuadrant(row, col)), boardState.getTurnPlayer());
+                }
+            }
+            count = 0;
+        }
+
+
+
+        return move;
     }
 
 }
